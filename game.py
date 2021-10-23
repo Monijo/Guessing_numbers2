f31a56fe2668db115998aad63864489318ec8637
Monijo <mokajozefowicz@gmail.com>
2021-10-23 18:58:44 +0200

    checking the answer

--- game.py
+++ game.py
@@ -1,6 +1,11 @@
 def users_inputs():
-    print("If your number is bigger print: bigger, if is lower print:lower and if I guess: you win!")
-    user_answer = input()
+    print("If your number is bigger print: bigger, if is lower print:lower and if I guess: you win ")
+    list_of_possibilities = ['bigger', 'lower', 'you win']
+    users_answer = input()
+    while users_answer not in list_of_possibilities:
+        print("Don't cheat me!")
+        users_answer= input()
+    return users_answer
 
 
 def guessing_number():
