cf68774249eff07fabb37cf7f66a5536fb0e846e
Monijo <mokajozefowicz@gmail.com>
2021-10-23 18:49:44 +0200

    users_inputs

--- game.py
+++ game.py
@@ -1,4 +1,16 @@
+def users_inputs():
+    print("If your number is bigger print: bigger, if is lower print:lower and if I guess: you win!")
+    user_answer = input()
+
+
 def guessing_number():
     print("Let's play!Imagine  number in the range 0-1000 and I will guess your number in max 10 attempts!")
     print("If you have one please press 'Enter' to continue!")
     input()
+    min = 0
+    max = 1000
+    user_answer = ""
+
+
+
+users_inputs()
