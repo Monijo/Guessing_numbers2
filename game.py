73d19bdc520fb7f64e97750d4d7e4e750e3c2c97
Monijo <mokajozefowicz@gmail.com>
2021-10-23 18:34:47 +0200

    rules

---
+++ game.py
@@ -0,0 +1,4 @@
+def guessing_number():
+    print("Let's play!Imagine  number in the range 0-1000 and I will guess your number in max 10 attempts!")
+    print("If you have one please press 'Enter' to continue!")
+    input()
